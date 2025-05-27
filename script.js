20999f959eff00ec7310123df0644ba631fa0ec5
mahfujul0189 <muslemuddin0189@gmail.com>
2025-05-27 23:41:50 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById("ad-popup").style.display = "none";
+    }, 5000);
+};
+
+function closeAd() {
+    document.getElementById("ad-popup").style.display = "none";
+}
